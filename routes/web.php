909bb64a425d26303cae366aa95d46f3d96ba9b6
Himel Majumder <himelmajumder1@gmail.com>
2024-03-04 16:18:42 +0100

    ✨ add middlewares

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Middleware\isAdmin;
+use App\Http\Middleware\isStudent;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IndexController;
 
@@ -18,9 +21,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'),'verified',])->group(function () {
-    Route::get('/dashboard', [IndexController::class, 'index'])->name('index.dashboard');
-    // Route::get('/dashboard', function () {
-    //     return view('dashboard');
-    // })->name('dashboard');
+// Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
+//     // Route::get('/dashboard', [IndexController::class, 'index'])->name('index.dashboard');
+//     // Route::get('/dashboard', function () {
+//     //     return view('dashboard');
+//     // })->name('dashboard');
+
+// });
+
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+    Route::middleware(isAdmin::class)->prefix('admin')->group(function () {
+        Route::view('/dashboard', 'admin.dashboard')->name('dashboard');
+
+    });
+
+    Route::middleware(isStudent::class)->prefix('student')->group(function () {
+        Route::view('/dashboard', 'student.dashboard')->name('dashboard');
+    });
+    
 });
+
+
