01551c76576479bc9d8118f954717d3673434519
MONLAU\himelmaj <himelmaj@campus.monlau.com>
2024-04-08 18:40:32 +0200

    delete dashboard student

--- resources/views/components/layouts/student/partials/sidebar.blade.php
+++ resources/views/components/layouts/student/partials/sidebar.blade.php
@@ -1,4 +1,4 @@
-<x-mary-menu activate-by-route class="">
+<x-mary-menu activate-by-route class="flex flex-1">
 
     <span class="ml-5 py-5 font-extrabold text-2xl flex items-center gap-2">
             <x-icons.student-icon class="w-6 h-6 inline" />
@@ -9,13 +9,8 @@
     <x-mary-menu-item title="Home" icon="o-home" link=" {{ route('student.dashboard')}} " />
     <x-mary-menu-item title="Chats" icon="o-chat-bubble-left-right" link=" {{ route('student.chat')}} " />
     <x-mary-menu-item title="Calendario" icon="o-calendar-days" link="{{ route('student.calendar')}}" />
-
     <x-mary-menu-item title="Theme" icon="o-swatch" @click="$dispatch('mary-toggle-theme')" />
 
-    {{--
-    @if (Laravel\Jetstream\Jetstream::managesProfilePhotos() == false)
-    <x-mary-menu-item title="Profile" icon="o-user" link="{{ route('profile.show') }}" />
-    @else
-    <x-mary-avatar :image="Auth::user()->profile_photo_path" :title="Auth::user()->username" class="!w-10 " />
-    @endif --}}
+
+
 </x-mary-menu>
--- resources/views/student/dashboard.blade.php
+++
@@ -1,3 +0,0 @@
-<x-app-layout>
-    <h1 class="bg-slate-500">Welcome STUDENT</h1>
-</x-app-layout>
