24c785132055b50215406b6eab2e9f56a4888be8
Rubén Martínez Alcántara <rubenmaralc@campus.monlau.com>
2024-04-08 18:49:59 +0200

    psychoheader

---
+++ resources/views/components/headerPsycho.blade.php
@@ -0,0 +1,7 @@
+<div class="border-2 border-red-600 w-screen bg-gradient-to-r from-green-200 to-blue-300">
+    <div>
+        <img src="" alt="">
+    </div>
+    <div></div>
+    <h1>PARTE HEADER PSYCHO</h1>
+</div>
