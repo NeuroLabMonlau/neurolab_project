876198a5761d3504c6d1680cebcc421cc0e38d34
MONLAU\himelmaj <himelmajumder1@gmail.com>
2024-04-09 19:04:37 +0200

    add vCalendar package vue, and create miniCalendar component

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,10 @@ import './bootstrap';
 
 import { createApp } from 'vue'
 import calendar from '../views/components/vue/calendar.vue'
+import minicalendar from '../views/components/vue/minicalendar.vue'
 
 window.app = createApp({
     components: {
-        calendar
+        calendar, minicalendar
     },
 }).mount('#app');
--- resources/views/web/sections/student/index.blade.php
+++ resources/views/web/sections/student/index.blade.php
@@ -1,5 +1,9 @@
 <x-layouts.student.app>
     <x-mary-header title="Bienvenido de nuevo {{ Auth::user()->username }}" size="text-3xl" with-anchor />
+
+    <div id="app">
+        <minicalendar/>
+    </div>
 </x-layouts.student.app>
 
 
