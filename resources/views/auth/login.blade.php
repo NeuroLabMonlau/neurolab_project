ecd61785197270136263a9a2eee5afccda678d7d
Marc Mera <marcmera19@gmail.com>
2024-05-07 20:23:28 +0200

    arregladas faltas de ortografia

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout >
     <div class="flex flex-row w-full p-4">
         <div class="w-1/2 ">
-            <img src="../../public/assets/img/logoOscar.jpg" alt="">
+            {{-- <img src="../../public/assets/img/logoOscar.jpg" alt=""> --}}
             <x-authentication-card-logo /> 
         </div>
         <div class="w-1/2 flex justify-end">
@@ -36,17 +36,10 @@
                         <x-input id="password" class="block mt-1 w-full text-green-400" type="password" name="password" required autocomplete="current-password"  placeholder="Contraseña" />
                     </div>
     
-                    <div class="block mt-4 flex justify-center">
-                        <label for="remember_me" class="flex items-center">
-                            <x-checkbox id="remember_me" name="remember" />
-                            <span class="ms-2 text-sm text-gray-600">{{ __('Recuerdame') }}</span>
-                        </label>
-                    </div>
-    
                     <div class="flex flex-col items-center justify-end mt-4">
                         @if (Route::has('password.request'))
                             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500" href="{{ route('password.request') }}">
-                                {{ __('¿Olvidaste la contraseña?') }}
+                                {{ __('¿Has olvidado la contraseña?') }}
                             </a>
                         @endif
     
@@ -57,7 +50,7 @@
 
                     
                     <div class="text-center mt-8 text-sm">
-                        <p class="terms_link font-semibold">Al continuar, estás aceptando los  <a href="" class="underline decoration-solid">Terminos y condiciones</a> y la <a href="" class="text-red-500 font-bold">politca de privacidad</a> de NeuraCrib</p>
+                        <p class="terms_link font-semibold">Al continuar, estás aceptando los  <a href="" class="underline decoration-solid">Términos y condiciones</a> y la <a href="" class="text-red-500 font-bold">polítca de privacidad</a> de <span class="text-blue-700">Meraki</span></p>
                     </div>
             </form>
         </div>
